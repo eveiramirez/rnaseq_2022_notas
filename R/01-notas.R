ce7790b3b7409c2faf3d3d2b9f79ef35bfd169be
eveiramirez <ignemmram@gmail.com>
2022-02-01 14:08:42 -0600

    Actualizacion de notas de clase 1 de feb

--- R/01-notas.R
+++ R/01-notas.R
@@ -1,7 +1,19 @@
-gitcreds::gitcreds_set()
-usethis::edit_git_config()
-usethis::use_git()
-usethis::use_github()
-usethis::gh_token_help()
-usethis::edit_r_environ()
-
+# Clase 01/02/2022
+#
+#
+# Ejercicio grupal
+#
+# BiodbUniprot
+#
+# Es muy útil dado a ser un repositorio que cuenta con gran cantidad de
+# información relacionada con proteínas.Cuenta con una descripción clara y
+# resumida del funcionamiento de la paquetería en su vignette, y funciona en
+# todas las plataformas, en las cuales no se reportan errores por el momento.
+#
+# Surfaltr
+#
+# En su vignette cuenta con una descripción muy precisa y detallada de las
+# funciones de la paquetería con varios ejemplos, y también explica sobre el
+# manejo de los datos que requiere y su instalación. Esta paquetería se
+# encuentra en todas las plataformas, y por el momento no se han reportado
+# algún error.
